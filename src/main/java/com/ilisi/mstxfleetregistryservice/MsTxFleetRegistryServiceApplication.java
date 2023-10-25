17d4937edada43c6f4f8dca7dc2c11c0ab8fffab
yassine <yassine@gmail.com>
2023-10-25 22:25:00 +0100

    first commit

---
+++ src/main/java/com/ilisi/mstxfleetregistryservice/MsTxFleetRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTxFleetRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTxFleetRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilisi/mstxfleetregistryservice/MsTxFleetRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
